929dcd02dc5e7fade66dd694924036fca73da24f
MahmoudYahia404 <100964958+MahmoudYahia404@users.noreply.github.com>
2022-10-28 21:21:26 +0200

    Update Nashville_Housing.sql

--- Nashville_Housing.sql
+++ Nashville_Housing.sql
@@ -13,7 +13,8 @@ MODIFY COLUMN SaleDate DATE;
 -- Add missing PropertyAddress
 UPDATE nashville_housing AS a
 JOIN nashville_housing AS b
-	ON a.ParcelID = b.ParcelID AND a.UniqueID != b.UniqueID
+	ON a.ParcelID = b.ParcelID AND 
+	   a.UniqueID != b.UniqueID
 SET A.PropertyAddress = IF(a.PropertyAddress = "", b.PropertyAddress, a.PropertyAddress)
 WHERE a.PropertyAddress = "" AND b.PropertyAddress != "";
 
@@ -21,8 +22,8 @@ WHERE a.PropertyAddress = "" AND b.PropertyAddress != "";
 -- Breaking out Address into Individual Columns (Address, City, State)
 SELECT 
 	PropertyAddress, 
-    SUBSTRING_INDEX(PropertyAddress, ', ', 1) , 
-    SUBSTRING_INDEX(PropertyAddress, ', ', -1) 
+        SUBSTRING_INDEX(PropertyAddress, ', ', 1) , 
+        SUBSTRING_INDEX(PropertyAddress, ', ', -1) 
 FROM nashville_housing;
 
 ALTER TABLE nashville_housing
@@ -30,7 +31,8 @@ ADD Property_Address VARCHAR(50) AFTER PropertyAddress,
 ADD Property_City VARCHAR(50) AFTER Property_Address;
 
 UPDATE nashville_housing
-SET Property_Address = SUBSTRING_INDEX(PropertyAddress, ', ', 1) , 
+SET 
+	Property_Address = SUBSTRING_INDEX(PropertyAddress, ', ', 1) , 
 	Property_City = SUBSTRING_INDEX(PropertyAddress, ', ', -1);
     
 ALTER TABLE nashville_housing
@@ -39,7 +41,7 @@ DROP COLUMN PropertyAddress;
 
 SELECT 
 	OwnerAddress,
-    SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ',', 1), ',', -1) AS Owner_Address,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ',', 1), ',', -1) AS Owner_Address,
 	SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ',', 2), ',', -1) AS Owner_Sity,
 	SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ',', 3), ',', -1) AS Owner_State
 FROM nashville_housing;
@@ -50,9 +52,10 @@ ADD Owner_Sity VARCHAR(50) AFTER Owner_Address,
 ADD Owner_State VARCHAR(2) AFTER Owner_Sity;
 
 UPDATE nashville_housing
-SET Owner_Address = SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ', ', 1), ', ', -1),
+SET
+	Owner_Address = SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ', ', 1), ', ', -1),
 	Owner_Sity = SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ', ', 2), ', ', -1),
-    Owner_State = SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ', ', 3), ', ', -1);
+	Owner_State = SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ', ', 3), ', ', -1);
     
 ALTER TABLE nashville_housing
 DROP COLUMN OwnerAddress;
@@ -77,17 +80,17 @@ CREATE VIEW cleand_nashville_housing AS
 SELECT *
 FROM (Select *,
 	ROW_NUMBER() OVER (
-	PARTITION BY ParcelID,
-				 Property_Address,
-				 SalePrice,
-				 SaleDate,
-				 LegalReference,
-                 Acreage,
-                 TotalValue,
-                 Bedrooms
-				 ORDER BY
-					UniqueID
-					) row_num
+		PARTITION BY ParcelID,
+		Property_Address,
+		SalePrice,
+		SaleDate,
+		LegalReference,
+                Acreage,
+                TotalValue,
+                Bedrooms
+		ORDER BY
+		UniqueID
+	        ) row_num
 From nashville_housing
 ) AS UniqueRows
 WHERE UniqueRows.row_num = 1;
